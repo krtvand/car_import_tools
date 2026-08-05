be181c5f562dd1ad5f4dfb96865cd8ba2a0d33b6
Andrey Kartaev <a.kartaev@qrator.net>
2026-08-05 18:20:17 +0300

    Part C

--- config.py
+++ config.py
@@ -73,10 +73,11 @@ class CarFilters:
 # Motors > Cars > Mazda > CX-30, 2018+, up to €25,000.
 DEFAULT_FILTERS = CarFilters(
     make="mazda",
-    model="cx-30",
-    year_min=2023,
-    mileage_min=0,
-    mileage_max=50000,
+    model="cx-5",
+    year_min=2022,
+    # year_max=2022,
+    mileage_min=30000,
+    mileage_max=70000,
 )
 
 
